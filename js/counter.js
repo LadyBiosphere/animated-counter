6a5a477ded7390816009e536f4bb54aa4b089845
Nishita <nishita.nz@gmail.com>
2018-01-11 21:18:04 +1300

    Add jQuery, Add animation.

--- js/counter.js
+++ js/counter.js
@@ -1,2 +1,14 @@
 // Always test to check for a connection
-console.log("Testing - JavaScript file has been linked correctly!");
+// console.log("Testing - JavaScript file has been linked correctly!");
+
+$('.count-this').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
