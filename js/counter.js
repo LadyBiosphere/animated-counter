fa9a9351df5322d8010b01eeee386c134a32b8b7
Nishita <nishita.nz@gmail.com>
2018-01-12 09:33:00 +1300

    Add annotations to JS.

--- js/counter.js
+++ js/counter.js
@@ -1,13 +1,18 @@
-// Always test to check for a connection
+// Always test to check for a connection!
 // console.log("Testing - JavaScript file has been linked correctly!");
 
+// Find the 'count this' class on the page and animate it
 $('.count-this').each(function () {
+
+	// Start the counting from a specified number - in this case, 0!
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 4000,
+    	// Speed of counter in ms, default animation style
+        duration: 2000,
         easing: 'swing',
         step: function (now) {
+        	// Round up the number
             $(this).text(Math.ceil(now));
         }
     });
